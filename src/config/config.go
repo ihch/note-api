532b5d580eccaa239ef4a43190d41434df3e4194
nemusou <ryubi.102395@gmail.com>
2020-01-05 21:29:12 +0900

    clean: gofmtした

--- src/config/config.go
+++ src/config/config.go
@@ -3,15 +3,15 @@ package config
 import "os"
 
 type DBConfig struct {
-  Database string
-  User string
-  Password string
+	Database string
+	User     string
+	Password string
 }
 
 func NewDBConfig() *DBConfig {
-  dbconfig := new(DBConfig)
-  dbconfig.Database = os.Getenv("MYSQL_DATABASE")
-  dbconfig.User = os.Getenv("MYSQL_USER")
-  dbconfig.Password = os.Getenv("MYSQL_PASSWORD")
-  return dbconfig
+	dbconfig := new(DBConfig)
+	dbconfig.Database = os.Getenv("MYSQL_DATABASE")
+	dbconfig.User = os.Getenv("MYSQL_USER")
+	dbconfig.Password = os.Getenv("MYSQL_PASSWORD")
+	return dbconfig
 }
